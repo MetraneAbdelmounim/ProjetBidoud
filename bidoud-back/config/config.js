fe5abe1c1525bfefb1dfdbb9958d8d8f0fdac582
MetraneAbdelmounim <abdelmounim.metrane@gmail.com>
2021-02-17 23:14:00 +0100

    Version finale with docker

--- bidoud-back/config/config.js
+++ bidoud-back/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
   HOST: process.env.HOST || '127.0.0.1',
   PORT: process.env.PORT || 5000,
-  bdUrl : "mongodb://127.0.0.1:27017/bidoudBD",
+  bdUrl : "mongodb://mongo:27017/bidoudBD",
   atlasBdUrl:"mongodb+srv://arram:arram@cluster0-3yvyb.mongodb.net/arramdb?retryWrites=true&w=majority",
   secret_token_key:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ",
   token_expiration :"24h"
