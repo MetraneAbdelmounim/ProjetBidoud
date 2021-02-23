3e7308b5c5ad8f2ad53e2293246e7e912e510edd
MetraneAbdelmounim <abdelmounim.metrane@gmail.com>
2021-02-23 10:04:19 +0100

    last

--- bidoud-back/config/config.js
+++ bidoud-back/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
   HOST: process.env.HOST || 'localhost',
   PORT: process.env.PORT || 5000,
-  bdUrl : "mongodb://mongodb:27017/bidoudBD",
+  bdUrl : "mongodb://mongo:27017/bidoudBD",
   atlasBdUrl:"mongodb+srv://aminebidoud:aminebidoud@cluster0.fzqjb.mongodb.net/bidoudBD?retryWrites=true&w=majority",
   secret_token_key:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ",
   token_expiration :"24h",
